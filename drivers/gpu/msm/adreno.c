cf25975869761dea3f41edef3a67949a58b612cc
Jordan Crouse <jcrouse@codeaurora.org>
2013-11-22 10:03:31 -0700

    msm: kgsl: Add an input handler to power up the GPU on a touch event
    On a device where the primary input is a touchscreen nobody is
    surprised that touch events are often the catalyst for drawing
    frames. If the GPU is in slumber we can get a jump on the action
    by watching for touch events and turning the GPU on in anticipation
    of a draw call. Register for events from all input drivers
    returning EV_ABS events and queue a work task to power up the GPU
    if it is currently in slumber.
    
    Change-Id: Ic0dedbadbdc96ef9ddd78252ea65f8e1cdd00110
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/adreno.c
    	drivers/gpu/msm/adreno.h

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -103,6 +103,7 @@ static const struct devfreq_governor_data adreno_governors[] = {
 	{ .name = "msm-adreno-tz", .data = &adreno_tz_data },
 };
 
+
 static const struct kgsl_functable adreno_functable;
 
 static struct adreno_device device_3d0 = {
@@ -1825,6 +1826,7 @@ adreno_probe(struct platform_device *pdev)
 	adreno_ft_init_sysfs(device);
 
 	kgsl_pwrscale_init(&pdev->dev, CONFIG_MSM_ADRENO_DEFAULT_GOVERNOR);
+
 
 
 	device->flags &= ~KGSL_FLAGS_SOFT_RESET;

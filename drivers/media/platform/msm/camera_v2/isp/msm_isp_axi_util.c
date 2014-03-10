970a1b2e30f89dac8c090ba0b3450d207effcf9a
Androguide <admin@androguide.fr>
2014-03-10 23:45:54 +0100

    msm: camera2: fix merge derp
    Change-Id: I662297c90499072a92ee06cdb73cb854887c9f28

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1429,24 +1429,24 @@ void msm_isp_process_axi_irq(struct vfe_device *vfe_dev,
 			if (stream_info->stream_type == BURST_STREAM)
 				stream_info->runtime_num_burst_capture--;
 
-				msm_isp_get_done_buf(vfe_dev, stream_info,
-							pingpong_status, &done_buf);
-				if (stream_info->stream_type == CONTINUOUS_STREAM ||
-					stream_info->runtime_num_burst_capture > 1) {
-					rc = msm_isp_cfg_ping_pong_address(vfe_dev,
-						stream_info, pingpong_status);
-				}
-				if (done_buf && !rc)
-					msm_isp_process_done_buf(vfe_dev,
-					stream_info, done_buf, ts);
-				if (done_buf && rc)
-					/* Propagate frame drop */
-					msm_isp_process_frame_drop(vfe_dev,
-					stream_info, ts);
+			msm_isp_get_done_buf(vfe_dev, stream_info,
+						pingpong_status, &done_buf);
+			if (stream_info->stream_type == CONTINUOUS_STREAM ||
+				stream_info->runtime_num_burst_capture > 1) {
+				rc = msm_isp_cfg_ping_pong_address(vfe_dev,
+					stream_info, pingpong_status);
 			}
 			if (done_buf && !rc)
 				msm_isp_process_done_buf(vfe_dev,
 				stream_info, done_buf, ts);
+			if (done_buf && rc)
+				/* Propagate frame drop */
+				msm_isp_process_frame_drop(vfe_dev,
+				stream_info, ts);
+				
+		    if (done_buf && !rc)
+			    msm_isp_process_done_buf(vfe_dev,
+			    stream_info, done_buf, ts);
 		}
 	}
 	return;
